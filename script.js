dc7a001cc77f21b9e2ceda396d592960236129cd
karim <karimcoder48@gmail.com>
2023-09-24 20:18:42 +0200

    new modifier

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let img = document.getElementById("img");
-let list =document.getElementById("list")
+let list =document.getElementById("list");
+let sp = document.getElementById("sp");
 img.addEventListener("click",()=>{
-   list.innerText = "ابوه : عبدالله \n جده : عبدالمطلب \n عمه : ابوطالب \n مكان ولادته : مكة المكرمة \n مولده : 12 ربيع 1 عام الفيل \n امه : امينة بنت وهب"
+   list.innerText = "ابوه : عبدالله \n جده : عبدالمطلب \n عمه : ابوطالب \n مكان ولادته : مكة المكرمة \n مولده : 12 ربيع 1 عام الفيل \n امه : امينة بنت وهب";
+   sp.style.display = "none";
 })
 
 img.addEventListener("dblclick",()=>{
   list.innerText = "";
+  sp.style.display = "block";
 })
 
